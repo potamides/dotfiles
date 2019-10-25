162be1963dd9c2277539fe04155d0c033d4b19e2
Jonas Belouadi <bigjonas96@gmail.com>
2019-10-25 17:06:46 +0200

    Disabled compositor as i don't need it anymore

--- config/awesome/autorun.sh
+++ config/awesome/autorun.sh
@@ -7,13 +7,6 @@ function run {
   fi
 }
 
-function run_last {
-  if ! pgrep $1 ;
-  then
-    exec $@&
-  fi
-}
-
 #run urxvtd -q -f -o
 run udiskie
 run redshift
@@ -21,4 +14,4 @@ run conky
 #compton -b --backend glx --glx-no-stencil --xrender-sync-fence --glx-no-rebind-pixmap --shadow-exclude 'focused || !focused' #to lazy to write a config
 #run blueman-applet
 #run indicator-kdeconnect
-run_last nm-applet
+run nm-applet
