976be0d7ffbbbb07e0a15baabcda8fb61cda2470
Jonas Belouadi <bigjonas96@gmail.com>
2020-04-27 16:14:19 +0200

    awesome: improve net-widget text

--- config/awesome/status-widgets/net-widget/init.lua
+++ config/awesome/status-widgets/net-widget/init.lua
@@ -11,7 +11,7 @@ local function read_wireless(stdout, interface)
     local signal_strength = math.floor(match / 70 * 100)
     local index           = math.min(4, signal_strength // 20)
 
-    return {signal_strength, "wireless_" .. index .. ".svg"}
+    return {signal_strength .. "%", "wireless_" .. index .. ".svg"}
   end
 end
 
@@ -19,9 +19,9 @@ local function read_wired(stdout)
   local match = stdout:match("up\n$")
 
   if match then
-    return {100, "wired.svg"}
+    return {"100%", "wired.svg"}
   else
-    return {0, "wired_na.svg"}
+    return {"OFF", "wired_na.svg"}
   end
 end
 
@@ -41,13 +41,13 @@ function net_widget.init(args)
     widget = wibox.widget.imagebox,
   }
   net_widget.text = wibox.widget.textbox()
-  net_widget.text:set_markup(string.format("<span color=%q><b>%s%%</b></span>", beautiful.bg_normal, 0))
+  net_widget.text:set_markup(string.format("<span color=%q><b>%s</b></span>", beautiful.bg_normal, "OFF"))
 
   net_widget.text = awful.widget.watch(cmd, args.timeout, function(widget, stdout)
     local result          = read_wireless(stdout, args.wireless_interface) or read_wired(stdout)
     local strength, image = table.unpack(result)
 
-    widget:set_markup(string.format("<span color=%q><b>%s%%</b></span>", beautiful.bg_normal, strength))
+    widget:set_markup(string.format("<span color=%q><b>%s</b></span>", beautiful.bg_normal, strength))
     net_widget.image:set_image(args.path_to_icons .. image)
     collectgarbage("step", 64)
   end)
