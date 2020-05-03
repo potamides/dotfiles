68a3cbf7448156f7b4bfc19d5fc8b1284d34bca4
Jonas Belouadi <bigjonas96@gmail.com>
2020-05-03 11:52:02 +0200

    switch terminal to alacritty

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -67,7 +67,7 @@ end
 
 
 -- This is used later as the default terminal and editor to run.
-local terminal         = "termite"
+local terminal         = "alacritty"
 local editor           = os.getenv("EDITOR") or "nvim"
 local editor_cmd       = terminal .. " -e " .. editor
 local browser          = "firefox"
