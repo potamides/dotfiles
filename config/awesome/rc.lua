d938d004dbcd759a0fe7732f9bbfaa77f4be1e79
Jonas Belouadi <bigjonas96@gmail.com>
2020-05-19 09:48:36 +0200

    awesome: incorporate fixes of modalawesome

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -13,9 +13,6 @@ local beautiful = require("beautiful")
 local naughty       = require("naughty")
 local menubar       = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
 -- other stuff
 local freedesktop  = require("freedesktop")
 local modalawesome = require("modalawesome")
