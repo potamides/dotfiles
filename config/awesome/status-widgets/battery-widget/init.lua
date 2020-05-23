0c2fb45aac26997ebf5559e85d6d944f5d3720f7
Jonas Belouadi <bigjonas96@gmail.com>
2020-05-23 17:28:20 +0200

    awesome: removed acpi dependency for battery widget

--- config/awesome/status-widgets/battery-widget/init.lua
+++ config/awesome/status-widgets/battery-widget/init.lua
@@ -1,6 +1,6 @@
 -------------------------------------------------
 -- Battery Widget for Awesome Window Manager
--- Shows the battery status using the ACPI tool
+-- Shows the battery status
 -- More details could be found here:
 -- https://github.com/streetturtle/awesome-wm-widgets/tree/master/battery-widget
 
@@ -14,20 +14,13 @@ local naughty = require("naughty")
 local watch = require("awful.widget.watch")
 local wibox = require("wibox")
 local gfs = require("gears.filesystem")
-local dpi = require('beautiful').xresources.apply_dpi
-
--- acpi sample outputs
--- Battery 0: Discharging, 75%, 01:51:38 remaining
--- Battery 0: Charging, 53%, 00:57:43 until charged
 
 local battery_widget = {}
 function battery_widget.init(args)
     args = args or {}
 
-    local path_to_icons = args.path_to_icons or awful.util.getdir("config").."themes/gruvbox/widgets/battery/"
+    local path_to_icons = args.path_to_icons or awful.util.getdir("config") .. "themes/gruvbox/widgets/battery/"
 
-    local display_notification = args.display_notification or false
-    local position = args.notification_position or "top_right"
     local enable_battery_warning = args.enable_battery_warning
     if enable_battery_warning == nil then
         enable_battery_warning = true
@@ -49,27 +42,6 @@ function battery_widget.init(args)
     battery_widget.text = wibox.widget.textbox()
     battery_widget.text:set_markup(string.format("<span color=%q><b>%s%%</b></span>", beautiful.bg_normal, 0))
 
-    -- Popup with battery info
-    -- One way of creating a pop-up notification - naughty.notify
-    local notification
-    local function show_battery_status(batteryType)
-        awful.spawn.easy_async([[bash -c 'acpi']],
-        function(stdout, _, _, _)
-            naughty.destroy(notification)
-            notification = naughty.notify{
-                text =  stdout,
-                title = "Battery status",
-                icon = path_to_icons .. batteryType .. ".svg",
-                icon_size = dpi(16),
-                position = position,
-                timeout = 5, hover_timeout = 0.5,
-                width = 200,
-                screen = mouse.screen
-            }
-        end
-        )
-    end
-
     -- Alternative to naughty.notify - tooltip. You can compare both and choose the preferred one
     --battery_popup = awful.tooltip({objects = {battery_widget}})
 
@@ -81,42 +53,34 @@ function battery_widget.init(args)
     local function show_battery_warning(time)
       naughty.notify({ preset = naughty.config.presets.critical,
               title = "Battery is low!",
-              text = string.format("About %sh left based on your usage.", time) })
+              text = string.format("About %smin left based on your usage.", time) })
     end
     local last_battery_check = os.time()
-    local batteryType = "battery-good-symbolic"
+    local batteryType        = "battery-good-symbolic"
+    local battery_path       = "/sys/class/power_supply/BAT%d/uevent"
 
-    watch("acpi -i", 15,
+    -- watch up to 3 batteries
+    watch(string.format("cat %s %s %s", battery_path:format(0), battery_path:format(1), battery_path:format(2)),  15,
     function(widget, stdout, _, _, _)
         local battery_info = {}
-        local capacities = {}
-        local status, charge_str, time
-        for s in stdout:gmatch("[^\r\n]+") do
-            status, charge_str, time = string.match(s, '.+: (%a+), (%d?%d?%d)%%,?(.*)')
-            if status ~= nil then
-                table.insert(battery_info, {status = status, charge = tonumber(charge_str)})
-            else
-                local cap_str = string.match(s, '.+:.+last full capacity (%d+)')
-                table.insert(capacities, tonumber(cap_str))
-            end
+        for status, power , energy, charge_str in stdout:gmatch('.*POWER_SUPPLY_STATUS=(%a+).+\
+POWER_SUPPLY_POWER_NOW=(%d+).+POWER_SUPPLY_ENERGY_NOW=(%d+).+POWER_SUPPLY_CAPACITY=(%d+)') do
+          table.insert(battery_info, {status = status, charge = charge_str, power = power, energy = energy})
         end
-
-        local capacity = 0
-        for _, cap in ipairs(capacities) do
-            capacity = capacity + cap
-        end
-
         local charge = 0
-        for i, cap in ipairs(capacities) do
-            local batt = battery_info[i]
-            if batt.charge >= charge then
-                status = batt.status -- use most charged battery status
-                -- this is arbitrary, and maybe another metric should be used
+        local time   = 0
+        local status = "Unknown"
+        for _, batt in pairs(battery_info) do
+            if batt.status == 'Charging' then
+                status = batt.status
+            elseif batt.status == 'Discharging' then
+              time = time + batt.energy / batt.power
             end
 
-            charge = charge + batt.charge * cap
+            charge = charge + batt.charge
         end
-        charge = charge / capacity
+        charge = charge / #battery_info
+        time   = 60 * time -- minutes
 
         battery_widget.text:set_markup(string.format("<span color=%q><b>%s%%</b></span>",
             beautiful.bg_normal, math.floor(charge)))
@@ -127,7 +91,7 @@ function battery_widget.init(args)
                 -- if 5 minutes have elapsed since the last warning
                 last_battery_check = os.time()
 
-                show_battery_warning(time)
+                show_battery_warning(math.floor(time))
             end
         elseif (charge >= 15 and charge < 40) then batteryType = "battery-caution%s-symbolic"
         elseif (charge >= 40 and charge < 60) then batteryType = "battery-low%s-symbolic"
@@ -148,11 +112,6 @@ function battery_widget.init(args)
         collectgarbage("step", 128)
     end,
     battery_widget.image)
-
-    if display_notification then
-        battery_widget.image:connect_signal("mouse::enter", function() show_battery_status(batteryType) end)
-        battery_widget.image:connect_signal("mouse::leave", function() naughty.destroy(notification) end)
-    end
 end
 
 return battery_widget
