126b9729bc08a664943e6364dd51229ce8fbd440
Jonas Belouadi <jonas.belouadi@posteo.net>
2021-04-03 14:59:45 +0200

    Fix "utils.shape not found" error in awesome.

---
+++ .config/awesome/utils/shape.lua
@@ -0,0 +1,39 @@
+local shape = {parallelogram = {}, rightangled = {}}
+
+function shape.parallelogram.right(cr, width, height, degree)
+    degree = degree or 10
+    cr:move_to(degree, 0)
+    cr:line_to(width, 0)
+    cr:line_to(width - degree, height)
+    cr:line_to(0, height)
+    cr:close_path()
+end
+
+function shape.parallelogram.left(cr, width, height, degree)
+    degree = degree or 10
+    cr:move_to(0, 0)
+    cr:line_to(width - degree, 0)
+    cr:line_to(width, height)
+    cr:line_to(degree, height)
+    cr:close_path()
+end
+
+function shape.rightangled.right(cr, width, height, degree)
+    degree = degree or 10
+    cr:move_to(degree, 0)
+    cr:line_to(width, 0)
+    cr:line_to(width, height)
+    cr:line_to(0, height)
+    cr:close_path()
+end
+
+function shape.rightangled.left(cr, width, height, degree)
+    degree = degree or 10
+    cr:move_to(0, 0)
+    cr:line_to(width - degree, 0)
+    cr:line_to(width, height)
+    cr:line_to(0, height)
+    cr:close_path()
+end
+
+return shape
