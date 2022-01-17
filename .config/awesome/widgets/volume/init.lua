0f2416c54b2e30657287d68f56995a32f935e94d
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-01-17 18:18:52 +0100

    awesome: improve volume widget

--- .config/awesome/widgets/volume/init.lua
+++ .config/awesome/widgets/volume/init.lua
@@ -2,9 +2,19 @@ local beautiful = require("beautiful")
 local wibox = require("wibox")
 local spawn = require("awful.spawn")
 
-local volume = {}
+local volume = {
+  volume = {
+    get = "pactl get-sink-volume @DEFAULT_SINK@",
+    set = "pactl set-sink-volume @DEFAULT_SINK@ %+d%%",
+  },
+  mute = {
+    get = "pactl get-sink-mute @DEFAULT_SINK@",
+    set = "pactl set-sink-mute @DEFAULT_SINK@ %s",
+  },
+  subscribe = "pactl subscribe"
+}
 
-local function set_volume(level)
+local function display_volume(level)
   local icon, state = "audio-volume-high-symbolic", level and (level .. "%") or "OFF"
 
   if not level then
@@ -22,56 +32,60 @@ local function set_volume(level)
 end
 
 local function update_widget()
-  spawn.easy_async("pactl get-sink-mute @DEFAULT_SINK@", function(mute)
+  spawn.easy_async(volume.mute.get, function(mute)
     if mute:match("yes") then
-      set_volume(false)
+      display_volume(false)
     else
-      spawn.easy_async("pactl get-sink-volume @DEFAULT_SINK@", function(vol)
-        set_volume(tonumber(vol:match("(%d+)%%")))
+      spawn.easy_async(volume.volume.get, function(vol)
+        display_volume(tonumber(vol:match("(%d+)%%")))
       end)
     end
   end)
 end
 
 local function subscribe()
-  local pid = spawn.with_line_callback("pactl subscribe", {stdout = function(event)
+  local pid = spawn.with_line_callback(volume.subscribe, {stdout = function(event)
     if event:match("change.*sink[^-]") then
       update_widget()
     end
   end})
 
   if type(pid) == "number" then
-    awesome.connect_signal("exit", function() spawn("kill " .. pid, false) end)
+    awesome.connect_signal("exit", function() awesome.kill(pid, awesome.unix_signal["SIGTERM"]) end)
   end
 end
 
 function volume.toggle()
-  spawn.easy_async("pactl set-sink-mute @DEFAULT_SINK@ toggle", update_widget)
+  spawn(volume.mute.set:format("toggle"), false)
 end
 
 function volume.raise()
-  spawn.easy_async("pactl set-sink-volume @DEFAULT_SINK@ +" .. volume.step .. "%", update_widget)
+  spawn.easy_async(volume.volume.get, function(vol)
+    local step = math.min(volume.step, math.max(volume.max - tonumber(vol:match("(%d+)%%")), 0))
+    spawn(volume.volume.set:format(step), false)
+  end)
 end
 
 function volume.lower()
-  spawn.easy_async("pactl set-sink-volume @DEFAULT_SINK@ -" .. volume.step .. "%", update_widget)
+  spawn(volume.volume.set:format(-volume.step), false)
 end
 
 function volume.init(args)
   args = args or {}
   volume.step = args.step or 5
+  volume.max = args.max or 130
   volume.path_to_icons = args.path_to_icons or beautiful.theme_path .. "/widgets/volume/"
 
   volume.text = wibox.widget.textbox()
   volume.image = wibox.widget.imagebox()
-  set_volume(false)
+  display_volume(false)
   update_widget()
   subscribe()
 
   volume.image:connect_signal("button::press", function(_, _, _, button)
-    if     (button == 4) then volume.raise()
-    elseif (button == 5) then volume.lower()
-    elseif (button == 1) then volume.toggle()
+    if     button == 4 then volume.raise()
+    elseif button == 5 then volume.lower()
+    elseif button == 1 then volume.toggle()
     end
   end)
 end
