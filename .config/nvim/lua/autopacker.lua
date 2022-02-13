a3adbd5c5e8128fbc428bb3a1091a2e1cddd2eae
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-02-14 00:20:06 +0100

    nvim: prevent autopacker from loading plugins before vimenter

--- .config/nvim/lua/autopacker.lua
+++ .config/nvim/lua/autopacker.lua
@@ -6,16 +6,35 @@
   neovim for the first time a pleasant experience.
 --]]
 
-local installed, packer = xpcall(function() return require("packer") end, function()
-  local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  vim.fn.system{'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path}
+local function require_and_install_packer()
+  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  vim.fn.system{'git', 'clone', '--depth', 1, 'https://github.com/wbthomason/packer.nvim', install_path}
   vim.cmd('packadd packer.nvim')
   return require("packer")
-end)
+end
+
+local already_installed, packer = xpcall(function() return require("packer") end, require_and_install_packer)
+local plugin_utils = require("packer.plugin_utils")
+local load_plugin = plugin_utils.load_plugin
+
+-- Monkey patch plugin loading method. By default packer sources plugins after
+-- installing them. This is fine when this happens after VimEnter, which is
+-- usually the case, however, here we install packages while loading the vimrc,
+-- and loading plugins before this is finished can lead to complications.
+-- Because of this we add a check to allow manual loading of plugins only after
+-- VimEnter. Ignoring plugin loading requests until then should be fine since
+-- vim sources plugins on its own after loading the vimrc, anyway.
+function plugin_utils.load_plugin(plugin)
+  if vim.v.vim_did_enter == 1 then
+    load_plugin(plugin)
+  elseif not plugin.opt then
+    vim.o.runtimepath = vim.o.runtimepath .. ',' .. plugin.install_path
+  end
+end
 
 function packer.autostartup(plugins)
   packer.startup(plugins)
-  if not installed then
+  if not already_installed then
     packer.sync()
     -- no option yet to run packer synchronously
     while not packer_plugins do
@@ -26,4 +45,3 @@ function packer.autostartup(plugins)
 end
 
 return packer
-
