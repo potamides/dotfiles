07c229806eba3708303fa9982bd4b976440a8c99
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-03-17 01:59:32 +0100

    neovim: remove netrw `gx` quickfix as bug is fixed upstream

--- .config/nvim/plugin/netrw.lua
+++ .config/nvim/plugin/netrw.lua
@@ -2,7 +2,8 @@
   Netrw quickfixes.
 --]]
 
--- works around an annoying netrw bug (see https://github.com/vim/vim/issues/4738)
-vim.api.nvim_set_keymap("n", "gx", "<cmd>call netrw#BrowseX(netrw#GX(), 0)<cr>", {noremap = true, silent = true})
+-- fix for 'gx' not opening URLs correctly (see https://github.com/vim/vim/issues/4738)
+--vim.api.nvim_set_keymap("n", "gx", "<cmd>call netrw#BrowseX(netrw#GX(), 0)<cr>", {noremap = true, silent = true})
+
 -- Prevent netrw from mapping bookmarks to 'gb' which would conflict with another mapping.
 vim.api.nvim_set_keymap('n', '<nop>', "<Plug>NetrwBookHistHandler_gb", {silent = true, nowait = true})
