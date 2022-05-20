9ade77e6c478713c530a02e88cd0e51b41ad66d7
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-05-20 22:41:33 +0200

    perf(neovim): improve performance of thesaurusfunc
    OpenOffice thesauri usually come with an index file which can be used for
    binary search. When this file is present we use it to speed up things.

--- .config/nvim/plugin/thesaurus.lua
+++ .config/nvim/plugin/thesaurus.lua
@@ -8,6 +8,53 @@
 
 vim.openoffice = {}
 
+-- Emulate lazy evaluation. Could be made better with __pairs, __ipairs and
+-- __len metatable events from lua 5.2+ (which we don't have in Neovim).
+local function lazy_table(iterator)
+  return setmetatable({}, {__index = function(tbl, idx)
+    while #tbl < idx do
+      table.insert(tbl, iterator())
+    end
+    return tbl[idx]
+  end})
+end
+
+local function find_start_byte(iterator, end_line, base)
+  local start_line, term, lines = 1, (base .. "|"):lower(), lazy_table(iterator)
+
+  while start_line <= end_line do
+    local line = math.floor((start_line + end_line) / 2)
+    if vim.startswith(lines[line], term) then
+      return tonumber(lines[line]:sub(#term + 1))
+    elseif vim.stricmp(base, vim.split(lines[line], "|")[1]) < 0 then
+      end_line = line - 1
+    else
+      start_line = line + 1
+    end
+  end
+  return -1
+end
+
+local function parse_entry(iterator, length, base)
+  local completions = {}
+  for _ = 1, length do
+    local synonyms = vim.split(iterator(), "|")
+    local pos = table.remove(synonyms, 1)
+    for _, synonym in ipairs(synonyms) do
+      -- remove additional information for to be inserted word which is often given in parentheses
+      local insert = vim.trim(synonym:gsub("%(.-%)", ""):gsub(" +", " "))
+      if vim.stricmp(base, insert) ~= 0 then
+        table.insert(completions, {
+          abbr = synonym,
+          word = insert,
+          menu = not vim.tbl_contains({"-", ""}, pos) and (pos:match("^%((.+)%)$") or pos) or nil
+        })
+      end
+    end
+  end
+  return completions
+end
+
 function vim.openoffice.thesaurusfunc(findstart, base)
   if findstart == 1 then
     local line, col = vim.api.nvim_get_current_line(), vim.api.nvim_win_get_cursor(0)[2]
@@ -15,29 +62,30 @@ function vim.openoffice.thesaurusfunc(findstart, base)
   end
 
   local completions, term = {}, base:lower() .. "|"
-  for _, file in ipairs(vim.opt.thesaurus:get()) do
-    if vim.fn.filereadable(file) == 1 then
-      local iterator = io.lines(file)
-      iterator() -- skip first line
+  for _, dat_file in ipairs(vim.opt.thesaurus:get()) do
+    -- thesaurus usually comes with an index file which can be used for binary
+    -- search (faster)
+    local idx_file, start_byte = vim.fn.fnamemodify(dat_file, ":r") .. ".idx"
+    if vim.fn.filereadable(idx_file) == 1 then
+      local idx_iter = io.lines(idx_file)
+      idx_iter() -- skip encoding info in first line
+      local length = tonumber(idx_iter())
+      start_byte = find_start_byte(idx_iter, length, base)
+    end
+
+    local entries = io.open(dat_file)
+    if entries and start_byte ~= -1 then
+      -- skip to first byte or after encoding info in first line
+      entries:seek("set", start_byte or #entries:read() + 1)
+      local iterator = entries:lines()
       for line in iterator do
         if vim.startswith(line, term) then
-          for _ = 1,tonumber(line:sub(#term + 1)) do
-            local synonyms = vim.split(iterator(), "|")
-            local pos = table.remove(synonyms, 1)
-            for _, synonym in ipairs(synonyms) do
-              -- remove additional information for to be inserted word which is often given in parentheses
-              local insert = vim.trim(synonym:gsub("%(.-%)", ""):gsub(" +", " "))
-              if insert ~= base then
-                table.insert(completions, {
-                  abbr = synonym,
-                  word = insert,
-                  menu = not vim.tbl_contains({"-", ""}, pos) and (pos:match("^%((.+)%)$") or pos) or nil
-                })
-              end
-            end
-          end
+          local length = tonumber(line:sub(#term + 1))
+          vim.list_extend(completions, parse_entry(iterator, length, base))
+          break
         end
       end
+      entries:close()
     end
   end
 
