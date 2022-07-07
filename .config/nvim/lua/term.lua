4c820d63d6d6cf5f47441ef8238b9912158f76b9
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-07-07 21:49:37 +0200

    feat(neovim): update term wrapper
    * add options for launching a terminal
    * support multiple terminal instances
    * add methods to close terminal

--- .config/nvim/lua/term.lua
+++ .config/nvim/lua/term.lua
@@ -1,41 +1,72 @@
 --[[
-Small :term wrapper for opening a terminal at the bottom of the screen with a persistent buffer.
+  Small :term wrapper for opening a terminal at the bottom of the screen with a
+  persistent buffer.
 --]]
 local term = {
   termwin = -1,
   termbuf = -1,
   height = 12,
-  augroup = vim.api.nvim_create_augroup("toggleterm", {})
+  augroup = vim.api.nvim_create_augroup("toggleterm", {}),
 }
 
-function term.open(cmd)
-  if not vim.api.nvim_win_is_valid(term.termwin) then
-    vim.cmd(('botright %dsplit'):format(term.height))
+function term:open(args)
+  if not vim.api.nvim_win_is_valid(self.termwin) then
+    vim.cmd(('botright %dsplit'):format(self.height))
     vim.opt_local.winfixheight = true
-    term.termwin = vim.api.nvim_get_current_win()
+    self.termwin = vim.api.nvim_get_current_win()
   else
-    vim.api.nvim_set_current_win(term.termwin)
-    vim.cmd(("wincmd J | resize %d"):format(term.height))
+    vim.api.nvim_set_current_win(self.termwin)
+    vim.cmd(("wincmd J | resize %d"):format(self.height))
   end
 
-  if not vim.api.nvim_buf_is_valid(term.termbuf) then
-    term.termbuf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_set_current_buf(term.termbuf)
-    vim.fn.termopen(cmd or vim.o.shell)
+  if not vim.api.nvim_buf_is_valid(self.termbuf) then
+    self.termbuf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_set_current_buf(self.termbuf)
+
+    if args.cmd ~= false then -- false means termopen is handled externally
+      vim.fn.termopen(args.cmd or vim.o.shell)
+    end
 
     vim.api.nvim_create_autocmd("BufEnter", {
-      group = term.augroup,
-      buffer = term.termbuf,
+      group = self.augroup,
+      buffer = self.termbuf,
       callback = function()
         if #vim.api.nvim_list_wins() == 1 then
           vim.cmd[[quit]]
         end
-      end}
-    )
+      end
+    })
   else
-    vim.api.nvim_set_current_buf(term.termbuf)
+    vim.api.nvim_set_current_buf(self.termbuf)
   end
-  vim.cmd[[startinsert]]
+
+  if args.nofocus then
+    vim.api.nvim_set_current_win(vim.fn.win_getid(vim.fn.winnr("#")))
+  elseif not args.noinsert then
+    vim.cmd[[startinsert]]
+  end
+
+  return self.termbuf, self.termwin
 end
 
-return term
+function term:close()
+  if vim.api.nvim_win_is_valid(self.termwin) then
+    vim.api.nvim_win_hide(self.termwin)
+  end
+end
+
+function term:destroy()
+  if vim.api.nvim_buf_is_valid(self.termbuf) then
+    vim.api.nvim_buf_delete(self.termbuf, {force = true})
+  end
+end
+
+-- create a terminal instance with its own buffer but same window
+function term.instance()
+  return setmetatable({termbuf = -1}, {
+    __index = term,
+    __newindex = function(_, k, v) term[k] = v end
+  })
+end
+
+return setmetatable(term, {__call = term.instance})
