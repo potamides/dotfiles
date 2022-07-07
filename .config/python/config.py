d8bc7a2651478b44d71556bff880d21820491225
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-07-07 21:48:23 +0200

    fix(python): create histfile when it doesn't exist

--- .config/python/config.py
+++ .config/python/config.py
@@ -1,17 +1,23 @@
-import sys
 from importlib.util import find_spec
-from os.path import join
-from os import getenv
+from os import getenv, makedirs
+from os.path import exists, join, dirname
+import sys
 
 # embed ptpython if it is installed and isn't the current REPL
-if "ptpython" not in getenv("_") and find_spec("ptpython"):
+if "ptpython" not in getenv("_", "") and find_spec("ptpython"):
     from ptpython.repl import embed, run_config
     from appdirs import user_config_dir, user_data_dir
 
     config_file = join(getenv("PTPYTHON_CONFIG_HOME", user_config_dir("ptpython", "prompt_toolkit")), "config.py")
     history_file = join(user_data_dir("ptpython", "prompt_toolkit"), "history")
+
+    # create histfile if it doesn't exist (else ptpython complains)
+    if not exists(history_file):
+        makedirs(dirname(history_file), exist_ok=True)
+        with open(history_file, 'w'): pass
+
     sys.exit(embed(globals(), locals(), lambda repl: run_config(repl, config_file), False, history_file))
 # if not customize standard python REPL
 else:
-    # make primary prompt shorter because ~/.inputrc shows vi editing mode
+    # make primary prompt shorter because ~/.config/readline/inputrc shows vi editing mode
     sys.ps1 = "> "
