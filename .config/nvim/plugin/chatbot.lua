5f5346bab65cdb0055a640a9f945d11e937a58ad
Jonas Belouadi <jonas.belouadi@posteo.net>
2023-09-01 12:33:52 +0200

    feat(nvim): update chatbot integration into nvim

--- .config/nvim/plugin/chatbot.lua
+++ .config/nvim/plugin/chatbot.lua
@@ -7,15 +7,22 @@ local chatterm = require("term").instance()
 vim.api.nvim_create_user_command("Chat", function(tbl)
   if vim.fn.executable("gpt") == 1 then
     chatterm:open{
-      cmd = {"gpt", unpack(tbl.fargs)},
-      opts={
-        env={
-          -- use ansi colors (looks better with vim colorscheme)
-          COLORTERM="standard",
-          TERM="xterm-16color"
-        }
-      }
+      -- we use neovim for syntax highlighting, so disable everything else
+      cmd = {"gpt", "--no_markdown", unpack(tbl.fargs)},
+      opts = {env = {NO_COLOR = "true"}}
     }
+
+    -- set syntax to markdown and enable concealing
+    vim.opt_local.syntax = "markdown"
+    vim.opt_local.concealcursor = 'nvc'
+    vim.opt_local.conceallevel = 2
+
+    -- add custom syntax highlighting for the prompt
+    local group, matchgroup = "GPTPrompt", "matchgroup=markdownBlockquote"
+    vim.cmd.syntax{args = {"region", group, matchgroup, [[start=">\s"]], [[end="$"]]}}
+    vim.cmd.syntax{args = {"region", group, matchgroup, 'start="multiline>"',  [[end="^\(\s\|\s\{11}\)\@!"]]}}
+    vim.cmd.syntax{args = {"clear", "markdownError", "markdownEscape", "luaError", "luaParenError"}}
+    vim.api.nvim_set_hl(0, group, {link = "String", default=true})
   else
     vim.notify(
       "gpt-cli not installed. Get it here: https://github.com/kharvd/gpt-cli",
