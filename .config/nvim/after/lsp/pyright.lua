7a727c291256f65c0713a680b66de8614eceb5fe
Jonas Belouadi <53401822+potamides@users.noreply.github.com>
2025-05-24 19:08:09 +0200

    fix(nvim): pyright integration

--- .config/nvim/after/lsp/pyright.lua
+++ .config/nvim/after/lsp/pyright.lua
@@ -23,19 +23,26 @@ return {
     end
   },
   on_init = function(client)
-    -- if there is a virtualenv in root dir, use it (only for pyright)
-    local pythonPath = vim.fs.joinpath(client.config.root_dir, ".venv/bin/python")
-    if vim.uv.fs_stat(pythonPath) and client.settings.python then
-      client.settings.python.pythonPath = pythonPath
+    local pythonPath = vim.fs.joinpath(client.config.root_dir or ".", ".venv/bin/python")
+    if vim.uv.fs_stat(pythonPath) then
+      -- if there is a virtualenv in root dir, use it (only for pyright)
+      if client.settings.python then
+        client.settings.python.pythonPath = pythonPath
+      end
       -- we also use the virtual env for debugpy
       for _, dapconf in ipairs(dap.configurations.python or {}) do
         dapconf.python = pythonPath
       end
     end
 
-    vim.keymap.set(
-      "n", "<localleader>or", '<cmd>LspPyrightOrganizeImports<cr>', {silent = true, buffer = true}
-    )
+    -- NOTE: cannot set on_attach directly as it would overwrite lspconfig one
+    local orig_on_attach = client.on_attach or function() end
+    function client.on_attach(id, bufnr)
+      orig_on_attach(id, bufnr)
+      vim.keymap.set(
+        "n", "<localleader>or", '<cmd>LspPyrightOrganizeImports<cr>', {silent = true, buffer = bufnr}
+      )
+    end
 
     -- disable LSP semantic highlighting (only relevant for basedpyright)
     client.server_capabilities.semanticTokensProvider = nil
