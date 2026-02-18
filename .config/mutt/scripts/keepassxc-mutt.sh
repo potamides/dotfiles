08c2b1781c23b5213f242f49b5e06a3e3541b679
Jonas Belouadi <jonas.belouadi@posteo.net>
2026-02-18 11:08:32 +0100

    feat(mutt): cache password with timeout

--- .config/mutt/scripts/keepassxc-mutt.sh
+++ .config/mutt/scripts/keepassxc-mutt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2086
+# shellcheck disable=SC2086,SC2155
 
 ###############################################################################
 #        Read keepassxc email attributes into a format mutt can source        #
@@ -7,19 +7,21 @@
 
 database=${KEEPASSXC_DATABASE:-~/database.kdbx}
 keyfile=${KEEPASSXC_KEYFILE:-} # optional
+timeout=600
 
 function get_secrets(){
-  local entry password secrets quiet IFS=$'\n'
+  local entry secrets passwd="$(keyctl print "%user:$0" 2>/dev/null)" IFS=$'\n'
 
   for entry in "$@"; do
     coproc keepassxc-cli show -a UserName -a Email -a Password -k "$keyfile" \
-      $quiet "$database" "$entry"
+      ${passwd:+--quiet} "$database" "$entry"
 
-    if [[ -z "$password" ]]; then
-      read -rs password
+    if [[ -z "$passwd" ]]; then
+      read -rs passwd
     fi
+
     exec 3>&"${COPROC[0]}"
-    echo "$password">&"${COPROC[1]}"
+    echo "$passwd">&"${COPROC[1]}"
     wait $COPROC_PID
     read -rd '' -u 3 -a secrets
 
@@ -29,16 +31,18 @@ function get_secrets(){
       return
     fi
 
-    quiet="--quiet" # don't show keepassxc prompt on subsequent entries
     echo "set my_${entry@L}_user=${secrets[0]@Q}"
     echo "set my_${entry@L}_mail=${secrets[1]@Q}"
     echo "set my_${entry@L}_pass=${secrets[2]@Q}"
   done
+
+  keyctl add user "$0" "$passwd" @u >/dev/null
+  keyctl timeout "%user:$0" "$timeout"
 }
 
 
 if [[ -n "$*" ]]; then
   # also exit mutt when receiving interrupt from keyboard
-  trap 'kill $PPID && exit' INT
+  trap 'kill %% $PPID && exit' INT
   get_secrets "$@"
 fi
