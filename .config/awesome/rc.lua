3b74984e5f7685e90609c43957b3ff1738a43929
Jonas Belouadi <jonas.belouadi@posteo.net>
2026-06-06 18:02:42 +0100

    fix(awesome): duplicate start-up error reporting

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,27 +41,12 @@ local unpack       = unpack or table.unpack -- luacheck: globals unpack (compati
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 naughty.connect_signal("request::display_error", function(message, startup)
-    naughty.notification {
-        urgency = "critical",
-        title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
-        message = message
-    }
+  naughty.notification{
+    urgency = "critical",
+    title   = "Oops, an error happened" .. (startup and " during startup!" or "!"),
+    message = message
+  }
 end)
-
--- Handle runtime errors after startup
-do
-  local in_error = false
-  awesome.connect_signal("debug::error", function(err)
-    -- Make sure we don't go into an endless error loop
-    if in_error then return end
-    in_error = true
-
-    naughty.notify({ preset = naughty.config.presets.critical,
-    title = "Oops, an error happened!",
-    text = tostring(err) })
-    in_error = false
-  end)
-end
 
 -- }}}
 -------------------------------------------------------------------------------
