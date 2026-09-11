c467a2d7ab0753160ae2e01e76da675f129eb248
Jonas Belouadi <jonas.belouadi@posteo.net>
2026-09-11 20:44:19 +0100

    fix(somewm): remove broken border color patch

--- .config/awesome/utils/patches.lua
+++ .config/awesome/utils/patches.lua
@@ -1,6 +1,5 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
-local protected_call = require("gears.protected_call")
 local timer = require("gears.timer")
 
 local patches = {fix = {}}
@@ -30,25 +29,6 @@ function patches.fix.stuck_lockscreen()
   end)
   awesome.connect_signal("lock::deactivate", function()
     if kb then kb:start() end
-  end)
-end
-
--- Lua config workaround for the red border flash on client startup
--- TODO: open issue/PR for this
-function patches.fix.red_border_flash()
-  timer.delayed_call(function()
-    local real_border_width = beautiful.border_width or 0
-
-    client.connect_signal("new", function()
-      -- Force C's get_border_width() (window.c:448, runs right after this) to read 0.
-      beautiful.border_width = 0
-    end)
-
-    client.connect_signal("request::manage", function(c)
-      -- Creation is done; restore the theme value and apply the real width.
-      beautiful.border_width = real_border_width
-      c.border_width = real_border_width
-    end)
   end)
 end
 
